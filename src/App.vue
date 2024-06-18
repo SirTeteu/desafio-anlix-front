0c482096345c080c8ba6ed5518932d46d5f548cf
Matheus Fernandes <matheusfcc2012@gmail.com>
2024-06-18 17:47:39 -0300

    adding sidemenu

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
 	<v-app>
-		<v-main>
-			<router-view></router-view>
-		</v-main>
+			<side-menu />
+
+			<v-main>
+				<router-view></router-view>
+			</v-main>
 	</v-app>
 </template>
 
 <script>
+import SideMenu from './components/SideMenu';
+
 export default {
 	name: 'App',
+
+	components: {
+		SideMenu
+	}
 }
 </script>
 
---
+++ src/components/SideMenu.vue
@@ -0,0 +1,37 @@
+<template>
+    <v-navigation-drawer 
+        color="teal-lighten-1"
+        permanent
+    >
+        <v-list density="compact" nav>
+            <v-list-item
+                v-for="page in pages"
+                :active="routeActive == page.name"
+                :prepend-icon="page.icon"
+                :title="page.name"
+                :value="page.name"
+            ></v-list-item>
+        </v-list>
+    </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+    name: "side-menu",
+
+    data() {
+        return {
+            pages: [
+                { name: 'Home', icon: 'mdi-home-city' }
+            ]
+        }
+    },
+
+    computed: {
+        routeActive() {
+            return this.$router.currentRoute.value.name;
+        }
+    }
+
+}
+</script>
